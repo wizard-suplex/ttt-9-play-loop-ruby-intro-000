113a0ca8ec65554da805fe59633fe993b995d91d
Alan Cooke <atc4@rice.edu>
2021-01-06 22:20:24 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,7 +38,7 @@ end
 # Define your play method below
 def play(board)
   turn_number = 0
-  while turn_number <= 9
+  while turn_number < 10
     turn_number += 1
     puts "Round " + turn_number.to_s + " Start!"
     turn(board)
